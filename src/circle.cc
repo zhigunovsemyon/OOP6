7856d8bb4fa9a4a744eaa6217485c1fa8727015c
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-03-25 18:04:48 +0300

    Создал класс Circle, пока без обработчика сообщений

---
+++ src/circle.cc
@@ -0,0 +1,58 @@
+#include "circle.h"
+#include "program.h"
+#include <SDL2/SDL_error.h>
+#include <SDL2/SDL_render.h>
+
+/*Рисование круга рисовальщиком rend, радиуса rad, c центром в pos, цвета col,
+с числом полигонов circle_presicion Возвращает 0 при штатной работе, либо 1*/
+static int DrawCircle(SDL_Renderer * rend,
+		      int rad,
+		      SDL_Point centre,
+		      SDL_Colour const * col,
+		      int circle_presicion)
+{
+	SDL_Vertex verts[3]; // 3 вершины полигона
+	verts[0].color = verts[1].color = verts[2].color = *col;
+
+	verts[0].tex_coord = verts[1].tex_coord = verts[2].tex_coord =
+		(SDL_FPoint){0, 0};
+
+	// Определение вершины, расположенной в центре круга на всех полигонах
+	verts[0].position = (SDL_FPoint){(float)centre.x, (float)centre.y};
+
+	for (int i = 0; i < circle_presicion; ++i) {
+		// Определение остальных вершин полигонов, сдвинутых на углы
+		//(i / кол-во полигонов) и (1+i / кол-во полигонов)
+		verts[1].position.x =
+			(float)centre.x +
+			(float)rad * SDL_cosf(2 * (float)M_PI * (float)(i + 1) /
+					      (float)circle_presicion);
+		verts[1].position.y =
+			(float)centre.y -
+			(float)rad * SDL_sinf(2 * (float)M_PI * (float)(i + 1) /
+					      (float)circle_presicion);
+		verts[2].position.x =
+			(float)centre.x +
+			(float)rad * SDL_cosf(2 * (float)M_PI * (float)i /
+					      (float)circle_presicion);
+		verts[2].position.y =
+			(float)centre.y -
+			(float)rad * SDL_sinf(2 * (float)M_PI * (float)i /
+					      (float)circle_presicion);
+
+		// Рисование каждого полигона, возврат кода ошибки при её
+		// возникновении
+		if (SDL_RenderGeometry(rend, NULL, verts, 3, NULL, 0))
+			return 1;
+	}
+	return 0;
+}
+
+void Circle::draw(SDL_Renderer * rend) const
+{
+	SDL_Colour const * used_col = &((selected_) ? sel_colour : def_colour);
+	if (DrawCircle(rend, radius_, pos_, used_col, polycount_))
+		throw Program::Error{Program::Error::type::SDL, SDL_GetError()};
+}
+
+void Circle::recieve_msg(Message &&) {}
---
+++ src/circle.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "graphic_obj.h"
+#include "message.h"
+
+class Circle : public GraphicObject {
+public:
+	void draw(SDL_Renderer *) const override;
+	void recieve_msg(Message &&) override;
+
+	Circle(int x, int y) : GraphicObject{x, y}, radius_{default_radius_} {}
+
+	~Circle() override = default;
+
+private:
+	constexpr static int default_radius_{64};
+	constexpr static int polycount_{18};
+
+	int radius_;
+};
