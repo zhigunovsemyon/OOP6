1928efb4a1f60b0332814507cfd106e2e9c0d55b
Semyon <zhigunovsemen@gmail.com>
2025-05-05 17:31:06 +0300

    Пофиксил гонку исключений при выбросе в конструкторе программы

--- src/main.cc
+++ src/main.cc
@@ -10,15 +10,15 @@ try {
 	return EXIT_SUCCESS;
 } catch (SDL_exception & err) {
 	std::cerr << "Ошибка SDL. Сообщение: " << err.what() << std::endl;
-	Program::get().quit();
+	Program::quit();
 	return EXIT_FAILURE;
 } catch (std::exception & err) {
 	std::cerr << "Поймано иное исключение. Сообщение: " << err.what()
 		  << std::endl;
-	Program::get().quit();
+	Program::quit();
 	return EXIT_FAILURE;
 } catch (...) {
 	std::cerr << "Поймано неизвестное исключение\n";
-	Program::get().quit();
+	Program::quit();
 	return EXIT_FAILURE;
 }
