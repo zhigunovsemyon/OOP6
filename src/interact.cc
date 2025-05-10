f488499e1c72be7e1a9b4dde9715679511b43de3
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-05-10 15:56:19 +0300

    Каждый интерактор выходит на клавишу; переделал базовый под switch

--- src/interact.cc
+++ src/interact.cc
@@ -14,26 +14,61 @@ void InteractCreate::click(SDL_Point const & point)
 	new Circle{point.x, point.y};
 }
 
-void InteractMove::kb_press(SDL_Scancode) const {}
-
-void InteractDelete::kb_press(SDL_Scancode) const {}
-
-void InteractCreate::kb_press(SDL_Scancode c) const {
+void InteractMove::kb_press(SDL_Scancode c) const
+{
 	if (SDL_SCANCODE_Q == c)
 		Program::get().send_msg(new MessageExit);
 	if (SDL_SCANCODE_ESCAPE == c)
-		Program::get().send_msg(
-			new MessageChmod{InteractBase::type()});
+		Program::get().send_msg(new MessageChmod{InteractBase::type()});
 }
 
-void InteractResize::kb_press(SDL_Scancode) const {}
+void InteractDelete::kb_press(SDL_Scancode c) const
+{
+	if (SDL_SCANCODE_Q == c)
+		Program::get().send_msg(new MessageExit);
+	if (SDL_SCANCODE_ESCAPE == c)
+		Program::get().send_msg(new MessageChmod{InteractBase::type()});
+}
+
+void InteractCreate::kb_press(SDL_Scancode c) const
+{
+	if (SDL_SCANCODE_Q == c)
+		Program::get().send_msg(new MessageExit);
+	if (SDL_SCANCODE_ESCAPE == c)
+		Program::get().send_msg(new MessageChmod{InteractBase::type()});
+}
+
+void InteractResize::kb_press(SDL_Scancode c) const
+{
+	if (SDL_SCANCODE_Q == c)
+		Program::get().send_msg(new MessageExit);
+	if (SDL_SCANCODE_ESCAPE == c)
+		Program::get().send_msg(new MessageChmod{InteractBase::type()});
+}
 
 void InteractBase::kb_press(SDL_Scancode c) const
 {
-	if (SDL_SCANCODE_C == c)
+	switch (c) {
+	case SDL_SCANCODE_Q:
+	case SDL_SCANCODE_ESCAPE:
+		Program::get().send_msg(new MessageExit);
+		break;
+	case SDL_SCANCODE_M:
+		Program::get().send_msg(new MessageChmod{InteractMove::type()});
+		break;
+	case SDL_SCANCODE_R:
+		Program::get().send_msg(
+			new MessageChmod{InteractResize::type()});
+		break;
+	case SDL_SCANCODE_D:
+		Program::get().send_msg(
+			new MessageChmod{InteractDelete::type()});
+		break;
+	case SDL_SCANCODE_C:
 		Program::get().send_msg(
 			new MessageChmod{InteractCreate::type()});
-
-	if (SDL_SCANCODE_Q == c || SDL_SCANCODE_ESCAPE == c)
-		Program::get().send_msg(new MessageExit);
+		break;
+	default:
+		break;
+	}
 }
