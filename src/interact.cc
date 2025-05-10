8118d38d46dece206e18d30626f27b7c2e2cebf6
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-05-10 14:49:48 +0300

    Добавил ресайзер, сделал поле типа у интерактора

--- src/interact.cc
+++ src/interact.cc
@@ -4,6 +4,8 @@ void InteractMove::click(SDL_Point const &) {}
 
 void InteractDelete::click(SDL_Point const &) {}
 
+void InteractResize::click(SDL_Point const &) {}
+
 #include "circle.h"
 void InteractCreate::click(SDL_Point const &point) 
 {
--- src/interact.h
+++ src/interact.h
@@ -7,11 +7,27 @@
 
 class InteractBase {
 public:
+	enum class Type {
+		MOVER,
+		RESIZER,
+		CREATOR,
+		DELETER
+	};
 	virtual ~InteractBase() = default;
 	virtual void click(SDL_Point const &) = 0;
+
+	InteractBase::Type type() const { return t_; }
+
+private:
+	Type t_;
 };
 
 class InteractMove : public InteractBase {
+public:
+	void click(SDL_Point const &) override;
+};
+
+class InteractResize : public InteractBase {
 public:
 	void click(SDL_Point const &) override;
 };
