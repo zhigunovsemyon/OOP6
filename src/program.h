521526f01e9eef12c198dfeedeaf922e801c5f02
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-05-11 18:08:47 +0300

    Сделал окно побольшее

--- src/program.h
+++ src/program.h
@@ -32,7 +32,7 @@
 class Program {
 private:	
 	static constexpr SDL_Colour bgcolour_{0x70, 0x70, 0x70, 0xFF};
-	static constexpr SDL_Point winsize_{640, 480};
+	static constexpr SDL_Point winsize_{960, 600};
 	static constexpr int frametime_{10};
 	static constexpr std::string_view WinName_{"Лабораторная работа №6"};
 
