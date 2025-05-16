4af1e87a395da7bb17a95ccbbea735f8f2a43215
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-05-16 13:06:17 +0300

    Рефактор rect.cc

--- src/rect.cc
+++ src/rect.cc
@@ -1,24 +1,29 @@
 #include "rect.h"
-#include "sdlexcept.h"
 #include "program.h"
+#include "sdlexcept.h"
 
 bool Rectangle::covers_(SDL_Point const & point) const noexcept
 {
-	if (point.x > pos_.x + wh_.x / 2 || point.x < pos_.x - wh_.x / 2)
+	bool const to_the_right = point.x > pos_.x + wh_.x / 2;
+	bool const to_the_left = point.x < pos_.x - wh_.x / 2;
+	if (to_the_right || to_the_left)
 		return false;
 
-	if (point.y > pos_.y + wh_.y / 2 || point.y < pos_.y - wh_.y / 2)
+	bool const above = point.y < pos_.y - wh_.y / 2;
+	bool const below = point.y > pos_.y + wh_.y / 2;
+	if (above || below)
 		return false;
 
 	return true;
 }
 
-void Rectangle::taller_() noexcept 
+void Rectangle::taller_() noexcept
 {
 	constexpr auto maxsize =
 		std::min(Program::winsize().y, Program::winsize().x) - step;
 
-	if ((wh_.y = (int)((float)wh_.y * mult_)) > maxsize)
+	wh_.y = (int)((float)wh_.y * mult_);
+	if (wh_.y > maxsize)
 		wh_.y = maxsize;
 }
 
@@ -27,15 +32,17 @@ void Rectangle::wider_() noexcept
 	constexpr auto maxsize =
 		std::min(Program::winsize().y, Program::winsize().x) - step;
 
-	if (wh_.x < maxsize - step)
-		wh_.x = (int)((float)wh_.x * mult_);
+	wh_.x = (int)((float)wh_.x * mult_);
+	if (wh_.x < maxsize)
+		wh_.x = maxsize;
 }
 
 void Rectangle::narrower_() noexcept
 {
 	constexpr auto minsize = 3 * step;
 
-	if ((wh_.x = (int)((float)wh_.x / mult_)) < minsize)
+	wh_.x = (int)((float)wh_.x / mult_);
+	if (wh_.x < minsize)
 		wh_.x = minsize;
 }
 
@@ -43,18 +50,20 @@ void Rectangle::lower_() noexcept
 {
 	constexpr auto minsize = 3 * step;
 
-	if ((wh_.y = (int)((float)wh_.y / mult_)) < minsize)
+	wh_.y = (int)((float)wh_.y / mult_);
+	if (wh_.y < minsize)
 		wh_.y = minsize;
 }
 
 void Rectangle::draw(SDL_Renderer * rend) const
 {
-	SDL_Rect const r{pos_.x - wh_.x / 2, pos_.y - wh_.y / 2, wh_.x, wh_.y};
+	SDL_Point const nw_corner{pos_.x - wh_.x / 2, pos_.y - wh_.y / 2};
+	SDL_Rect const r{nw_corner.x, nw_corner.y, wh_.x, wh_.y};
 
-	if(SDL_SetRenderDrawColor(rend, cur_colour().r, cur_colour().g,
-			       cur_colour().b, cur_colour().a))
+	if (SDL_SetRenderDrawColor(rend, cur_colour().r, cur_colour().g,
+				   cur_colour().b, cur_colour().a))
 		throw SDL_exception{};
-			       
+
 	if (SDL_RenderFillRect(rend, &r))
 		throw SDL_exception{};
 }
