07381e5f876f76e6a6d8a5e875c54cec281dca31
Semyon <zhigunovsemen@gmail.com>
2025-05-20 14:02:50 +0300

    Законстил ловлю исключений

--- src/main.cc
+++ src/main.cc
@@ -8,13 +8,13 @@ int main(int, char *[])
 try {
 	Program::get()();
 	return EXIT_SUCCESS;
-} catch (SDL_exception & err) {
+} catch (SDL_exception const & err) {
 	std::cerr << "Ошибка SDL. Сообщение: " << err.what() << std::endl;
 	return EXIT_FAILURE;
-} catch (TTF_exception & err) {
+} catch (TTF_exception const & err) {
 	std::cerr << "Ошибка SDL_ttf. Сообщение: " << err.what() << std::endl;
 	return EXIT_FAILURE;
-} catch (std::exception & err) {
+} catch (std::exception const & err) {
 	std::cerr << "Поймано иное исключение. Сообщение: " << err.what()
 		  << std::endl;
 	return EXIT_FAILURE;
