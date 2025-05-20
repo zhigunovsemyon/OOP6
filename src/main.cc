4fe4bd5c9a1a13495e7f77813ef5b6a5a5652f23
Semyon <zhigunovsemen@gmail.com>
2025-05-20 14:07:17 +0300

    Переделал ловлю ошибок

--- src/main.cc
+++ src/main.cc
@@ -1,24 +1,14 @@
 #include "program.h"
-#include "sdlexcept.h"
-
-#include <cassert>
-#include <iostream>
+#include <print>
 
 int main(int, char *[])
 try {
 	Program::get()();
 	return EXIT_SUCCESS;
-} catch (SDL_exception const & err) {
-	std::cerr << "Ошибка SDL. Сообщение: " << err.what() << std::endl;
-	return EXIT_FAILURE;
-} catch (TTF_exception const & err) {
-	std::cerr << "Ошибка SDL_ttf. Сообщение: " << err.what() << std::endl;
-	return EXIT_FAILURE;
 } catch (std::exception const & err) {
-	std::cerr << "Поймано иное исключение. Сообщение: " << err.what()
-		  << std::endl;
+	std::println(stderr, "Ошибка с сообщением: {}", err.what());
 	return EXIT_FAILURE;
 } catch (...) {
-	std::cerr << "Поймано неизвестное исключение\n";
+	std::println(stderr, "Неизветная ошибка");
 	return EXIT_FAILURE;
 }
