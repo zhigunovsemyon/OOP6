8e14374517fd2b9fa0b36e86053cbf32a2da3019
Semyon <zhigunovsemen@gmail.com>
2025-05-20 14:13:07 +0300

    Убрал исключения TTF

--- src/sdlexcept.cc
+++ src/sdlexcept.cc
@@ -5,11 +5,6 @@
 #include <SDL2/SDL_error.h>
 #endif // SDL_error_h_
 
-#include <SDL_ttf.h>
-#ifndef SDL_TTF_H_
-#include <SDL2/SDL_ttf.h>
-#endif // !SDL_TTF_H_
-
 SDL_exception::SDL_exception() noexcept : std::runtime_error{SDL_GetError()} {}
 
 SDL_exception::SDL_exception(char const * str) noexcept
@@ -19,11 +14,3 @@ SDL_exception::SDL_exception(char const * str) noexcept
 
 SDL_exception::~SDL_exception() = default;
 
-TTF_exception::TTF_exception() noexcept : std::runtime_error{TTF_GetError()} {}
-
-TTF_exception::TTF_exception(char const * str) noexcept
-	: std::runtime_error{str}
-{
-}
-
-TTF_exception::~TTF_exception() = default;
